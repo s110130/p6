f6ee51a78aa0c3f7629336af759412c0fea6d1a6
s110130 <131227327+s110130@users.noreply.github.com>
2023-05-24 10:23:08 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("： )")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
